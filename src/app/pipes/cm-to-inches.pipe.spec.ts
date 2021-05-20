9767a500f9e6c25165c5ef2fab0199ab34ad210e
Nina Iarkova <nina@symmetry.com>
2021-05-20 18:19:38 -0400

    Auto-generated commit

--- src/app/pipes/cm-to-inches.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CmToInchesPipe } from './cm-to-inches.pipe';
-
-describe('CmToInchesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CmToInchesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
